bbb49f76f32bec925c0d3021585a8ba9587ed25e
oncdastan <oncdastan@gmail.com>
2022-12-28 02:53:47 +0300

    Initial project add

---
+++ src/test/java/ocd/springframework/dependencyinjectionexamples/DependencyInjectionExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package ocd.springframework.dependencyinjectionexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
