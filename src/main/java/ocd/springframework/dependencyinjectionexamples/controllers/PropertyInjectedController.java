0b9113ccf4f71c51c6323f5fcd487fb4c1f00e9e
oncdastan <oncdastan@gmail.com>
2023-07-31 00:46:34 +0300

    example(EX03): dependency injection without spring - part 2

--- src/main/java/ocd/springframework/dependencyinjectionexamples/controllers/PropertyInjectedController.java
+++ src/main/java/ocd/springframework/dependencyinjectionexamples/controllers/PropertyInjectedController.java
@@ -1,11 +1,14 @@
 package ocd.springframework.dependencyinjectionexamples.controllers;
 
 import ocd.springframework.dependencyinjectionexamples.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
     // LEAST preferred method
-
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting() {
